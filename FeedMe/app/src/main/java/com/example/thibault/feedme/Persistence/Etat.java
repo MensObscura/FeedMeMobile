5f5b32e79041ffd23d6dfc2e4c63f4172a93ff17
Thibault le plus beau <thibaultrosa@gmail.fr>
2015-09-25 15:52:20 +0200

    ormlite the beginning

---
+++ FeedMe/app/src/main/java/com/example/thibault/feedme/Persistence/Etat.java
@@ -0,0 +1,37 @@
+package com.example.thibault.feedme.Persistence;
+
+import com.j256.ormlite.field.DatabaseField;
+
+/**
+ * Created by Thibault on 19/09/2015.
+ */
+public class Etat {
+
+
+
+
+    @DatabaseField(id= true)
+    String code;
+    @DatabaseField
+    String nom;
+
+
+    Etat() {
+        // needed by ormlite
+    }
+
+    Etat(String code, String nom) {
+
+        this.code = code;
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("code : ").append(this.code);
+        sb.append(", ").append("Nom : ").append(this.nom);
+
+        return sb.toString();
+    }
+}
